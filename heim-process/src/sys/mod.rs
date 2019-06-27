7240ec55befd3930dc209ab6d4866a381ad9f1a8
svartalf <self@svartalf.info>
2019-06-27 13:38:34 +0300

    Properly feature-gating the OS-specific implementations for process crate

--- heim-process/src/sys/mod.rs
+++ heim-process/src/sys/mod.rs
@@ -1,13 +1,17 @@
-mod linux;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        mod linux;
 
-pub use self::linux::*;
+        pub use self::linux::*;
+    } else if #[cfg(target_os = "macos")] {
+        mod macos;
 
-//cfg_if::cfg_if! {
-//    if #[cfg(target_os = "linux")] {
-//        mod linux;
-//
-//        pub use self::linux::*;
-//    } else {
-//        compile_error!("Unsupported target OS");
-//    }
-//}
+        pub use self::macos::*;
+    } else if #[cfg(target_os = "windows")] {
+        mod windows;
+
+        pub use self::windows::*;
+    } else {
+        compile_error!("Unsupported target OS");
+    }
+}
