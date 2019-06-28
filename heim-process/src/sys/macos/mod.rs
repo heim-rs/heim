b81eb535b5de6a6192c76ccee1156ad43e3b1890
svartalf <self@svartalf.info>
2019-06-28 16:54:04 +0300

    process::pids for macOS (closes #47)

--- heim-process/src/sys/macos/mod.rs
+++ heim-process/src/sys/macos/mod.rs
@@ -1,3 +1,4 @@
+mod bindings;
 mod pids;
 
 pub use self::pids::*;
--- heim-process/src/sys/macos/pids.rs
+++ heim-process/src/sys/macos/pids.rs
@@ -1,10 +1,20 @@
 use heim_common::prelude::*;
 
 use crate::Pid;
+use super::bindings::processes;
 
 pub fn pids() -> impl Stream<Item = Result<Pid>> {
-    // TODO: Stub, see https://github.com/heim-rs/heim/issues/47
-    stream::iter(vec![])
+    future::lazy(|_| {
+        // `kinfo_proc` is not `Send`-able, so it would not be possible
+        // later to send it between threads (it's full of raw pointers),
+        // so for MVP we are just going to collect all the pids in-place.
+        let pids = processes()?.into_iter()
+            .map(|proc| Ok(proc.kp_proc.p_pid))
+            .collect::<Vec<_>>();
+
+        Ok(stream::iter(pids))
+    })
+    .try_flatten_stream()
 }
 
 pub fn pid_exists(_pid: Pid) -> impl Future<Output = Result<bool>> {
