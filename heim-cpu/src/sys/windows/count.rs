93c5a505b821f879fae017b29b02d547e50f17a9
svartalf <self@svartalf.info>
2019-07-11 10:27:57 +0300

    cpu::logical_count for Windows (closes #51)

--- heim-cpu/src/sys/windows/count.rs
+++ heim-cpu/src/sys/windows/count.rs
@@ -1,9 +1,19 @@
+use winapi::um::{winbase, winnt};
+
 use heim_common::prelude::*;
 
 
 pub fn logical_count() -> impl Future<Output = Result<u64>> {
     // TODO: Stub, see https://github.com/heim-rs/heim/issues/51
-    future::ok(1)
+    let result = unsafe {
+        winbase::GetActiveProcessorCount(winnt::ALL_PROCESSOR_GROUPS)
+    };
+
+    if result > 0 {
+        future::ok(u64::from(result))
+    } else {
+        future::err(Error::last_os_error())
+    }
 }
 
 pub fn physical_count() -> impl Future<Output = Result<Option<u64>>> {
