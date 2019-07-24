976edbbd1099b8a70816c4dab07b18aababd7b05
svartalf <self@svartalf.info>
2019-07-24 14:16:32 +0300

    Fixing possible buffer overflow at cpu::times for macOS (closes #78)

--- heim-cpu/src/sys/macos/bindings.rs
+++ heim-cpu/src/sys/macos/bindings.rs
@@ -3,11 +3,12 @@
 use std::mem;
 use std::ptr;
 use std::ffi::CStr;
+use std::slice;
 
 use mach::kern_return::{self, kern_return_t};
 use mach::traps::mach_task_self;
 use mach::mach_port;
-use mach::vm_types::{natural_t, integer_t};
+use mach::vm_types::{natural_t, integer_t, vm_map_t, vm_address_t, vm_size_t};
 use mach::message::mach_msg_type_number_t;
 use mach::mach_types::host_t;
 
@@ -17,6 +18,10 @@ use heim_common::prelude::*;
 const PROCESSOR_CPU_LOAD_INFO: libc::c_int = 2;
 const HOST_CPU_LOAD_INFO: libc::c_int = 3;
 const HOST_VM_INFO: libc::c_int = 2;
+const CPU_STATE_USER: usize = 0;
+const CPU_STATE_SYSTEM: usize = 1;
+const CPU_STATE_IDLE: usize = 2;
+const CPU_STATE_NICE: usize = 3;
 
 type processor_flavor_t = libc::c_int;
 type processor_info_array_t = *mut integer_t;
@@ -87,7 +92,13 @@ extern "C" {
         flavor: processor_flavor_t,
         out_processor_count: *mut natural_t,
         out_processor_info: *mut processor_info_array_t,
-        out_processor_infoCnt: *const mach_msg_type_number_t,
+        out_processor_infoCnt: *mut mach_msg_type_number_t,
+    ) -> kern_return_t;
+
+    fn vm_deallocate(
+        target_task: vm_map_t,
+        address: vm_address_t,
+        size: vm_size_t,
     ) -> kern_return_t;
 }
 
@@ -122,30 +133,47 @@ pub unsafe fn cpu_load_info() -> Result<host_cpu_load_info> {
 #[allow(trivial_casts)]
 pub unsafe fn processor_load_info() -> Result<Vec<processor_cpu_load_info>> {
     let port = macos::mach_host_self();
-    let mut stats: Vec<processor_cpu_load_info> = Vec::with_capacity(1);
-    // TODO: Move to const
-    let count = mem::size_of::<processor_cpu_load_info>() / mem::size_of::<integer_t>();
-    let mut result_count: natural_t = 0;
+    let mut cpu_count = 0;
+    let mut processor_info: processor_info_array_t = ptr::null_mut();
+    let mut cpu_info_count = 0;
 
     let result = host_processor_info(
         port,
         PROCESSOR_CPU_LOAD_INFO,
-        &mut result_count,
-        &mut stats.as_mut_ptr() as *mut _  as *mut processor_info_array_t,
-        &count as *const _ as *const mach_msg_type_number_t
+        &mut cpu_count,
+        &mut processor_info,
+        &mut cpu_info_count,
     );
 
     let port_result = mach_port::mach_port_deallocate(mach_task_self(), port);
-    // Technically it is a programming bug and we are should panic probably,
-    // but it is okay as is
     if port_result != kern_return::KERN_SUCCESS {
-        return Err(Error::last_os_error());
+        return Err(Error::last_os_error())
     }
 
     if result != kern_return::KERN_SUCCESS {
         Err(Error::last_os_error())
     } else {
-        stats.set_len(result_count as usize);
+        let cpu_info = slice::from_raw_parts(processor_info, cpu_info_count as usize);
+        // Could use a `::std::mem::transmute` probably, but this is okay too
+        let mut stats = Vec::with_capacity(cpu_count as usize);
+        for chunk in cpu_info.chunks(4) {
+            stats.push(processor_cpu_load_info {
+                user: chunk[CPU_STATE_USER] as natural_t,
+                system: chunk[CPU_STATE_SYSTEM] as natural_t,
+                idle: chunk[CPU_STATE_IDLE] as natural_t,
+                nice: chunk[CPU_STATE_NICE] as natural_t,
+            })
+        }
+
+        let result = vm_deallocate(
+            mach_task_self(),
+            processor_info as vm_address_t,
+            cpu_info_count as vm_size_t * std::mem::size_of::<natural_t>(),
+        );
+        if result != kern_return::KERN_SUCCESS {
+            return Err(Error::last_os_error())
+        }
+
         Ok(stats)
     }
 }
