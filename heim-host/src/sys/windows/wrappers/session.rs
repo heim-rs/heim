1adbc2b97b11d9a82f0ccab0615258748a87b0a9
svartalf <self@svartalf.info>
2019-08-07 00:41:58 +0300

    Rework Windows user sessions pointer arithmetic (closes #93)

--- heim-host/src/sys/windows/wrappers/session.rs
+++ heim-host/src/sys/windows/wrappers/session.rs
@@ -93,3 +93,4 @@ impl Session {
         Ok(address)
     }
 }
+
--- heim-host/src/sys/windows/wrappers/sessions.rs
+++ heim-host/src/sys/windows/wrappers/sessions.rs
@@ -1,4 +1,5 @@
-use std::mem;
+use std::ptr;
+use std::slice;
 
 use winapi::um::winnt::PVOID;
 use winapi::shared::minwindef::DWORD;
@@ -8,17 +9,17 @@ use heim_common::prelude::*;
 use super::super::bindings::wtsapi32;
 use super::Session;
 
-/// Iterator over user sessions
-pub struct Sessions {
-    info: wtsapi32::WTS_SESSION_INFOW,
+/// Iterator over user sessions.
+pub struct Sessions<'s> {
+    info: &'s mut [wtsapi32::WTS_SESSION_INFOW],
     current: DWORD,
     count: DWORD,
 }
 
-impl Sessions {
+impl<'s> Sessions<'s> {
     #[allow(trivial_casts)]
-    pub fn new() -> Result<Sessions> {
-        let mut info = mem::MaybeUninit::<wtsapi32::WTS_SESSION_INFOW>::uninit();
+    pub fn new() -> Result<Sessions<'s>> {
+        let mut info = ptr::null_mut();
         let mut count: DWORD = 0;
 
         let result = unsafe {
@@ -27,7 +28,7 @@ impl Sessions {
                 wtsapi32::WTS_CURRENT_SERVER_HANDLE,
                 0,
                 1,
-                &mut &mut info as *mut &mut _ as *mut wtsapi32::PWTS_SESSION_INFOW,
+                &mut info,
                 &mut count,
             )
         };
@@ -35,12 +36,12 @@ impl Sessions {
         if result == 0 {
             Err(Error::last_os_error())
         } else {
-            let info = unsafe {
-                info.assume_init()
+            let sessions = unsafe {
+                slice::from_raw_parts_mut(info, count as usize)
             };
 
             Ok(Sessions {
-                info,
+                info: sessions,
                 count,
                 current: 0,
             })
@@ -51,31 +52,31 @@ impl Sessions {
 // `wtsapi32::WTS_SESSION_INFOW` contains pointers in it,
 // but since we are responsible to free this struct later,
 // I think it is okay to `Send` it?
-unsafe impl Send for Sessions {}
+unsafe impl<'s> Send for Sessions<'s> {}
 
-impl Drop for Sessions {
+impl<'s> Drop for Sessions<'s> {
     #[allow(trivial_casts)]
     fn drop(&mut self) {
         unsafe {
-            wtsapi32::WTSFreeMemory(&mut self.info as *mut _ as PVOID)
+            // https://docs.microsoft.com/en-us/windows/win32/api/wtsapi32/nf-wtsapi32-wtsfreememory
+            wtsapi32::WTSFreeMemory(self.info.as_mut_ptr() as *mut _ as PVOID)
         }
     }
 }
 
-impl Iterator for Sessions {
+impl<'s> Iterator for Sessions<'s> {
     type Item = Session;
 
     #[allow(trivial_casts)]
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current >= self.count {
-            None
-        } else {
-            let session: wtsapi32::WTS_SESSION_INFOW = unsafe {
-                *(&mut self.info as wtsapi32::PWTS_SESSION_INFOW).offset(self.current as isize)
-            };
+        if self.current < self.count {
+            let session = self.info.get(self.current as usize)
+                .expect("Invalid session index");
             self.current += 1;
 
             Some(Session::new(session.SessionId))
+        } else {
+            None
         }
     }
 }
