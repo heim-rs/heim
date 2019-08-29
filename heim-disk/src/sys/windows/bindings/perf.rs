4090afb6bf65aa4a1a5535ec1918487c148e65df
svartalf <self@svartalf.info>
2019-08-29 16:35:02 +0300

    Using winapi DISK_PERFORMANCE struct instead of bundled one (closes #62)

--- heim-disk/src/sys/windows/bindings/perf.rs
+++ heim-disk/src/sys/windows/bindings/perf.rs
@@ -12,28 +12,6 @@ use heim_common::prelude::*;
 const ERROR_INVALID_FUNCTION: i32 = winerror::ERROR_INVALID_FUNCTION as i32;
 const ERROR_NOT_SUPPORTED: i32 = winerror::ERROR_NOT_SUPPORTED as i32;
 
-// Is not declared in the `winapi`
-// TODO: Get rid of it when the winapi-rs PR will be merged
-// https://github.com/retep998/winapi-rs/pull/765
-// See https://github.com/heim-rs/heim/issues/62
-#[repr(C)]
-#[derive(Default)]
-#[allow(non_snake_case)]
-pub struct DISK_PERFORMANCE {
-    pub BytesRead: winnt::LARGE_INTEGER,
-    pub BytesWritten: winnt::LARGE_INTEGER,
-    pub ReadTime: winnt::LARGE_INTEGER,
-    pub WriteTime: winnt::LARGE_INTEGER,
-    pub IdleTime: winnt::LARGE_INTEGER,
-    pub ReadCount: minwindef::DWORD,
-    pub WriteCount: minwindef::DWORD,
-    pub QueueDepth: minwindef::DWORD,
-    pub SplitCount: minwindef::DWORD,
-    pub QueryTime: winnt::LARGE_INTEGER,
-    pub StorageDeviceNumber: minwindef::DWORD,
-    pub StorageManagerName: [winnt::WCHAR; 8],
-}
-
 /// ## Returns
 ///
 /// `DeviceIoControl` might fail in some rare and hardly reproducible conditions.
@@ -42,7 +20,7 @@ pub struct DISK_PERFORMANCE {
 /// For reference: https://github.com/giampaolo/psutil/blob/5a398984d709d750da1fc0e450d72c771e18f393/psutil/_psutil_windows.c#L2262-L2277
 #[allow(trivial_casts)]
 #[allow(clippy::trivially_copy_pass_by_ref)]
-pub fn disk_performance(volume_path: &Path) -> Result<Option<DISK_PERFORMANCE>> {
+pub fn disk_performance(volume_path: &Path) -> Result<Option<winioctl::DISK_PERFORMANCE>> {
     let os_str = volume_path.as_os_str();
     let mut path = os_str.encode_wide().collect::<Vec<_>>();
     // Dropping out trailing backslash and replacing it with a \0
@@ -67,7 +45,7 @@ pub fn disk_performance(volume_path: &Path) -> Result<Option<DISK_PERFORMANCE>> 
         return Err(Error::last_os_error())
     }
 
-    let mut perf = DISK_PERFORMANCE::default();
+    let mut perf = winioctl::DISK_PERFORMANCE::default();
     let mut bytes_returned: minwindef::DWORD = 0;
 
     let result = unsafe {
@@ -77,7 +55,7 @@ pub fn disk_performance(volume_path: &Path) -> Result<Option<DISK_PERFORMANCE>> 
             ptr::null_mut(),
             0,
             &mut perf as *mut _ as minwindef::LPVOID,
-            mem::size_of::<DISK_PERFORMANCE>() as minwindef::DWORD,
+            mem::size_of::<winioctl::DISK_PERFORMANCE>() as minwindef::DWORD,
             &mut bytes_returned,
             ptr::null_mut(),
         )
