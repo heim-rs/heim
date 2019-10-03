396a2c31012db484132061e6679b1f8b3f7845f5
svartalf <self@svartalf.info>
2019-10-03 20:28:30 +0300

    cpu::logical_count for macOS

--- heim-cpu/src/sys/macos/count.rs
+++ heim-cpu/src/sys/macos/count.rs
@@ -5,7 +5,7 @@ use std::ptr;
 use heim_common::prelude::*;
 
 #[allow(trivial_casts)]
-fn sysctl(key: &[u8]) -> Result<u64> {
+fn sysctl(key: &[u8]) -> Result2<u64> {
     let str = unsafe { CStr::from_bytes_with_nul_unchecked(key) };
     let mut value = 0i32;
     let mut length = mem::size_of::<i32>();
@@ -23,14 +23,14 @@ fn sysctl(key: &[u8]) -> Result<u64> {
     if result == 0 {
         Ok(value as u64)
     } else {
-        Err(Error::last_os_error())
+        Err(Error2::last_os_error())
     }
 }
 
-pub fn logical_count() -> impl Future<Output = Result<u64>> {
-    future::ready(sysctl(b"hw.logicalcpu\0"))
+pub async fn logical_count() -> Result2<u64> {
+    sysctl(b"hw.logicalcpu\0")
 }
 
 pub fn physical_count() -> impl Future<Output = Result<Option<u64>>> {
-    future::ready(sysctl(b"hw.physicalcpu\0").map(Some))
+    future::ready(sysctl(b"hw.physicalcpu\0").map(Some).map_err(Into::into))
 }
