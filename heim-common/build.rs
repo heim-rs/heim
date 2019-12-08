3191d6660f0734ca0e6b7daf7e9534494b55f8b0
svartalf <self@svartalf.info>
2019-12-08 18:12:41 +0300

    Enforcing new clippy lints

--- heim-common/build.rs
+++ heim-common/build.rs
@@ -1,10 +1,7 @@
 use std::env;
 
 fn main() {
-    match env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
-        Ok("macos") => {
-            println!("cargo:rustc-link-lib=framework=IOKit");
-        }
-        _ => {}
+    if let Ok("macos") = env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
+        println!("cargo:rustc-link-lib=framework=IOKit");
     }
 }
