3848144bb25b1f13c5b6bb694f628325a05726e5
Jet Spark <lixiaopeng.jetspark@bytedance.com>
2021-10-08 12:54:53 +0800

    Fix incorrect CpuTime for macOS of M1 processor

--- heim-process/src/sys/macos/process/cpu_times.rs
+++ heim-process/src/sys/macos/process/cpu_times.rs
@@ -1,4 +1,5 @@
 use heim_common::units::{time, Time};
+use heim_host::TIME_BASE;
 
 #[derive(Debug, Clone)]
 pub struct CpuTime {
@@ -19,8 +20,8 @@ impl CpuTime {
 impl From<darwin_libproc::proc_taskinfo> for CpuTime {
     fn from(info: darwin_libproc::proc_taskinfo) -> CpuTime {
         CpuTime {
-            utime: Time::new::<time::nanosecond>(info.pti_total_user as f64),
-            stime: Time::new::<time::nanosecond>(info.pti_total_system as f64),
+            utime: Time::new::<time::nanosecond>(info.pti_total_user as f64 * *TIME_BASE),
+            stime: Time::new::<time::nanosecond>(info.pti_total_system as f64 * *TIME_BASE),
         }
     }
 }
